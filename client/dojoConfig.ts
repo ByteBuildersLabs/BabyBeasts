7e2fd7cff4a014d5b79b6c763a571f9959fb7577
RolandoDrRobot <rolando.arguello.email@gmail.com>
2024-11-03 19:24:44 -0600

    Set slot

--- client/dojoConfig.ts
+++ client/dojoConfig.ts
@@ -4,4 +4,8 @@ import manifest from "./src/manifest_dev.json";
 
 export const dojoConfig = createDojoConfig({
     manifest,
+    masterAddress: "0x6044d4dbbb271bd4392f84b49f1bf593cbf9d5eadb657bfedbbe475daeaf182",
+    masterPrivateKey: "0x3b912ca72fc70b8b8937b43a9367a67f34e725299605a325df264b1cf8fd41d",
+    rpcUrl: "https://api.cartridge.gg/x/bytebeast-stable/katana",
+    toriiUrl: "https://api.cartridge.gg/x/bytebeast-stable/torii",
 });
